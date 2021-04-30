904267b470f0c6d053b2730518555807bec3de6b
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-04-30 14:50:16 +0800

    1.1.4
    chg shows

--- fp-play-tools/for-kafka/fake-produce-aslike-consumed/kmsg-sending-replayer.sh
+++ fp-play-tools/for-kafka/fake-produce-aslike-consumed/kmsg-sending-replayer.sh
@@ -43,7 +43,7 @@ awk '!/\\N/' "$file" |
             echo "$thatdaycurrent_x" | awk '{ print $3 }' ;
 
             echo "$thatdaycurrent_x" |
-                awk '{ printf "'"$turnpassedtimes"':"$1","strftime("[%T%z],senddone!",$2/1000,"")" " }' >&2 ;
+                awk '{ printf "send,'"$turnpassedtimes"':"$1","strftime("[%T%z]",$2/1000,"")",done " }' >&2 ;
         } ;
     done ;
 
